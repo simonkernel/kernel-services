5441e0feb99b613a63581402e8550a04395746f9
rorysaur <ou.rory@gmail.com>
2022-05-04 22:57:08 -0400

    remove sidebar component from portal app

--- packages/wallet/src/components/Page.jsx
+++ packages/wallet/src/components/Page.jsx
@@ -8,22 +8,18 @@
 
 import { FooterSmall, Navbar } from '@kernel/common'
 
-import Sidebar from 'components/Sidebar'
 import AppConfig from 'App.config'
 
 const Page = ({ children }) => {
   return (
     <>
-      <Sidebar />
-      <div className='relative md:ml-64 bg-blueGray-100'>
-        <Navbar
-          title={AppConfig.appTitle}
-          backgroundColor='bg-kernel-dark' textColor='text-kernel-white'
-        />
-        {children}
-        <div className='relative md:pt-32 pb-32 pt-12'>
-          <FooterSmall absolute />
-        </div>
+      <Navbar
+        title={AppConfig.appTitle}
+        backgroundColor='bg-kernel-dark' textColor='text-kernel-white'
+      />
+      {children}
+      <div className='relative md:pt-32 pb-32 pt-12'>
+        <FooterSmall absolute />
       </div>
     </>
   )
